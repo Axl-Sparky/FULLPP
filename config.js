f68be4cf91ab9b27ee43ba2191fc3ff86632d3f5
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2024-12-12 19:28:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PREFIX: process.env.PREFIX || "𝐂𝐫𝐨𝐧𝐞𝐱𝐁𝐨𝐭~",
+  PREFIX: process.env.PREFIX || "OCTA-BASE~",
   EMAIL: process.env.EMAIL || "ameenxnt@gmail.com",
   PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
   MESSAGE: process.env.MESSAGE || "_Thank you for using our bot, dont share your session id with anyone!_",
