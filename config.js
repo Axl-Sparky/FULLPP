1af01631074e97a01973c5551a5071640f60993e
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2024-12-12 15:08:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   PREFIX: process.env.PREFIX || "𝐂𝐫𝐨𝐧𝐞𝐱𝐁𝐨𝐭~",
   EMAIL: process.env.EMAIL || "ameenxnt@gmail.com",
   PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  MESSAGE: process.env.MESSAGE || "_Thank you for using our bot, dont share your session id with anyone!_",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
