f5315a27a750a24e8c8fd02dffcb7228f0a2a0da
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2024-12-12 15:09:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   EMAIL: process.env.EMAIL || "ameenxnt@gmail.com",
   PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
   MESSAGE: process.env.MESSAGE || "_Thank you for using our bot, dont share your session id with anyone!_",
-  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
+  IMAGE: process.env.IMAGE || "https://files.catbox.moe/m445ih.jpeg",
 }
