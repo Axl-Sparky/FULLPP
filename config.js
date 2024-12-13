4dd1739c8aa352f313181ccf95653d0f658d11c6
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2024-12-13 10:10:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   PREFIX: process.env.PREFIX || "OCTA-BASE~",
   EMAIL: process.env.EMAIL || "ameenxnt@gmail.com",
   PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
-  MESSAGE: process.env.MESSAGE || "_Thank you for using our bot, dont share your session id with anyone!_",
+  MESSAGE: process.env.MESSAGE || "_Thank you for using NEXTRO-MD, dont share your session id with anyone!_",
   IMAGE: process.env.IMAGE || "https://files.catbox.moe/m445ih.jpeg",
 }
